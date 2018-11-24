3047e08d5ef2a5cd561a9296ec67f7880a98c425
Riley Adair <hello@rileyadair.com>
2018-11-24 12:15:29 -0600

    Backup og files

---
+++ src/App-og.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app" :class="deviceType">
+    <Header />
+      <!-- router -->
+        <Home :items="items" />
+      <!-- End router -->
+  </div>
+</template>
+
+<script>
+import States from './core/services/States'
+import items from './core/collections/items'
+import Header from './components/Header'
+import Home from './pages/Home'
+
+export default {
+  name: 'app',
+  components: {
+    Header,
+    Home
+  },
+  data() {
+    return {
+      deviceType: States.deviceType,
+      items: items,
+    }
+  },
+}
+</script>
