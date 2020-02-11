4cd78639a79d5a5a9c20968c35e124d4b1cf1609
Riley Adair <rileydadair@gmail.com>
2020-02-10 21:42:14 -0600

    Fix first Reveal overflow

--- src/components/Reveal.vue
+++ src/components/Reveal.vue
@@ -1,6 +1,6 @@
 <template>
   <div :class="className" ref="reveal">
-    <div :class="`${className}__wrap`">
+    <div :class="[`${className}__wrap`, { 'has-entered': hasEntered }]">
       <div :class="[`${className}__inner`, 'js-hover', { 'is-portrait': isPortrait }, { 'is-clickable': isClickable }]" data-reveal ref="inner" v-on:click="onPhotoClick">
         <div :class="`${className}__img`" class="brand-img" :style="{ backgroundImage: bgImage }" ref="img"></div>
         <div :class="`${className}__overlay ${className}__overlay--top`" ref="overlayTop"></div>
@@ -30,7 +30,8 @@ export default {
         mobileDuration: 1.2,
         ease: CustomEase.create("custom", "M0,0 C0.204,0.04 0.266,0.101 0.314,0.214 0.358,0.318 0.358,0.34 0.396,0.48 0.428,0.6 0.432,0.71 0.522,0.816 0.6,0.908 0.704,1 1,1"),
         mobileEase: CustomEase.create("custom", "M0,0 C0.29,0 0.312,0.111 0.348,0.166 0.381,0.216 0.414,0.34 0.446,0.48 0.466,0.57 0.492,0.756 0.582,0.862 0.66,0.954 0.704,1 1,1")
-      }
+      },
+      hasEntered: false
     }
   },
   computed: {
@@ -76,6 +77,9 @@ export default {
         ease: this.animation.ease,
         startAt: {scale: 1.1 },
         scale: 1,
+        onComplete: () => {
+          this.hasEntered = true;
+        }
       })
     }
   }
